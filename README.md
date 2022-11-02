fb05de0c33639ca60e0be21a970aa93d945d247f
fmassin <fmassin@sed.ethz.ch>
2022-11-02 13:13:11 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Hypo71 configuration for VolcSAT
+
+Add the content of `global.cfg` to your `~/.seiscomp/global.cfg`:
+
+```bash
+cat global.cfg >> ~/.seiscomp/global.cfg
+```
---
+++ run.sh
@@ -0,0 +1,8 @@
+HYPO71PC_BINARY=Hypo71PC
+HYPO71PC_HOME=`dirname $0`
+
+# Jumping into the right directory
+cd ${HYPO71PC_HOME}/
+
+# Executing binary with input file as argument
+${SEISCOMP_ROOT}/bin/$HYPO71PC_BINARY < input
